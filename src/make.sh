961aa2d9bffe0475bc77ada6e7b7db75b214705d
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-12 03:12:10 +0900

    Use Python

--- src/make.sh
+++ src/make.sh
@@ -3,9 +3,9 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
-ruby convert_neologd_to_mozcdic.rb
-ruby adjust_entries.rb mozcdic-ut-neologd.txt
-ruby filter_unsuitable_words.rb mozcdic-ut-neologd.txt
+python convert_neologd_to_mozcdic.py
+python adjust_entries.py mozcdic-ut-neologd.txt
+python filter_unsuitable_words.py mozcdic-ut-neologd.txt
 
 tar cjf mozcdic-ut-neologd.txt.tar.bz2 mozcdic-ut-neologd.txt
 mv mozcdic-ut-neologd.txt* ../
